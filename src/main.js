d7ce78c5b9952afa31e64e47823d395642120a83
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-30 23:37:06 -0700

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Date from './weekday-calculator.js';
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function () {
